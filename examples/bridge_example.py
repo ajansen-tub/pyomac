817ac9212a019147b0fad153197ad1d976032d79
Patrick Simon <mail@patricksimon.info>
2021-07-27 15:55:39 +0200

    bridge example altered - work in progress

--- examples/bridge_example.py
+++ examples/bridge_example.py
@@ -6,17 +6,18 @@ from pyomac.plot import ssi_stability_plot, fdd_peak_picking_plot
 
 def main():
 
-    method = 'fdd'  # 'ssi' or 'fdd'
+    method = 'ssi'  # 'ssi' or 'fdd'
 
     with open('sample.csv') as csv_file:
         data = np.loadtxt(csv_file, skiprows=1, delimiter=',')
+        # data.shape: (n_samples x n_channels) = (90000 x 18) 
 
     fs = 100  # sampling frequency [Hz]
     fmax = 25.
 
     if method == 'ssi':
         fn, ms, zeta, poles_stab, order_stab, poles, order = ssi_cov(data, fs, f_max=fmax, return_poles=True)
-        ssi_stability_plot(data, fs, fn, poles_stab, order_stab, poles, order)
+        fig, ax = ssi_stability_plot(data, fs, fn, poles_stab, order_stab, poles, order)
 
     elif method == 'fdd':
         f, s, u = fdd(data, fs, fmax=25)
@@ -31,7 +32,7 @@ def main():
     idx_n = [0, 1, 2, 6, 7, 8, 12, 13, 14]
     idx_s = [3, 4, 5, 9, 10, 11, 15, 16, 17]
 
-    for idx, val in enumerate(fn):
+    for idx, val in enumerate(fn)[:3]:
         fig, ax = plt.subplots()
 
         ax.plot(ms[idx, idx_n])
